56409d654ae46e49697de894d5d9936a20b87c8b
Your Name01~git <saifsahai2002@gmail.com>
2025-07-19 10:16:10 +0600

    Tailwind configured

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import './App.css'
 
 function App() {
 
+    // const str = "Sabrina"  
+    // const splittedString = str.split("").reverse().join("")
+    // console.log(splittedString);
+
+  // let arr = [,,,]
+  // console.log(arr.length);
+  
+
+
   return (
     <div>
-      <h1>Redux-Toolkit</h1>
+      <h1 className='text-3xl font-bold underline'>Redux-Toolkit</h1>
     </div>
   )
 }
